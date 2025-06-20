9aae8447ba4b527aa8ee192f576cb2ef1a307633
Pablojonh6550 <pablojonh6550@gmail.com>
2025-06-20 15:22:37 -0300

    feat: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, CommonModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
